492ac94892883453ac64497ec87ae1ce0b60b9d8
Anatsrisaputh <jane.stbc@gmail.com>
2020-04-18 22:59:38 +0700

    Learn React Todolist

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const inputValue = (e) => setInput(e.target.value);
+
+  const add = () => {
+    setTodoList([...todoList, { id: todoList.length + 1, value: input }])
+    setInput("")
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+
+        <div><h1>Todo List</h1> </div>
+        <br />
+
+        <div>
+
+          <ul>
+            {todoList.map(obj => <li key={obj.id}> {obj.value} </li>)}
+          </ul>
+
+        </div>
+        <br />
+
+        <div>
+          <input onChange={inputValue} value={input} />
+          <button onClick={add}>Add Todo List</button>
+        </div>
+      </div>
     </div>
   );
 }

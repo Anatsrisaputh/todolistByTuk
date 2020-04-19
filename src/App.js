715d449e06e57557a62fce4ed79d35296204f2f3
Anatsrisaputh <jane.stbc@gmail.com>
2020-04-19 21:03:51 +0700

    Update Learn react hook todo list

--- src/App.js
+++ src/App.js
@@ -4,27 +4,48 @@ import './App.css';
 function App() {
   const [input, setInput] = useState("");
   const [todoList, setTodoList] = useState([]);
+  // const [deleteButton, setDeleteButton] = useState();
 
-  const inputValue = (e) => setInput(e.target.value);
+  const inputValue = (e) => {
+    setInput(e.target.value);
+
+  }
 
   const add = () => {
     setTodoList([...todoList, { id: todoList.length + 1, value: input }])
     setInput("")
-
   }
+
+  const deleteInput = (index) => {
+    // console.log({ index })
+    // let deleteFilter = [...todoList]
+    // deleteFilter = deleteFilter.filter((item,idx)=> idx !== index)
+    // // console.log(deleteAdd)
+    // setTodoList(deleteFilter)
+
+    let deleteFilter2 = [...todoList]
+    deleteFilter2.splice(index, 1)
+    setTodoList(deleteFilter2)
+    // การลบด้วย.splice(สั้นกว่า)
+  }
+
 
 
   return (
     <div>
       <div className="App">
 
-        <div><h1>Todo List</h1> </div>
+        <div>
+          <h1>Todo List</h1>
+        </div>
         <br />
 
         <div>
 
           <ul>
-            {todoList.map(obj => <li key={obj.id}> {obj.value} </li>)}
+            {todoList.map((obj, index) =>
+              <li key={obj.id}> {obj.value} <button onClick={() => deleteInput(index)}> Delete </button>  </li>
+            )}
           </ul>
 
         </div>
@@ -34,6 +55,7 @@ function App() {
           <input onChange={inputValue} value={input} />
           <button onClick={add}>Add Todo List</button>
         </div>
+
       </div>
     </div>
   );
